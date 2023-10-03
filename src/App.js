7e3d3515785d4729b27a9a9117e1ce327875cc12
Asma Zahra <asmazahra7860@gmail.com>
2023-10-03 10:57:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className=" font-bold text-red-500 text-3xl">
+        <p>
+let's build Netflix GPT
+
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
